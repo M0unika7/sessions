1d25155ed710ddf95eb4bb2a09296327fbda90e0
mouni <97968235+M0unika7@users.noreply.github.com>
2022-03-04 16:36:39 +0530

    Add files via upload

---
+++ session4.js
@@ -0,0 +1,121 @@
+// let a = 5;
+// let b = 6;
+//  console.log(a^b)
+
+//  0 1 0 1
+//  0 1 1 0
+//  0 0 1 1
+// answer is 3
+
+// let a = 5;
+// let b = 6;
+//  console.log(~a);
+
+
+//two conditions
+//decision making-->
+    //if-else -->
+// let a = 10;
+// if (a==10){
+//     console.log(success);
+// }
+// else{
+//     console.log(failure);
+// }
+
+
+// three conditions
+   //if-else ladder -->
+// let a = -10;
+// if(a<0) {
+//     negative
+// }
+// else if (a==0){
+//     zero
+// }
+// else{
+//     positive
+// }
+//..............................................
+//three or more conditions
+//switch-case statement -->
+// let gender = "m";
+// switch(gender){
+//     case "m":
+//         console.log("male");
+//         break;
+//     case "f":
+//         female
+//         break;
+//     default:
+//         transgender
+//         break;
+// }
+//................................................
+// let age = "21";
+//  if(age<18){
+//      console.log("child");
+//  }
+//  else if(18<age<40){
+//      console.log("adult");
+//  }
+//  else{
+//      console.log("old age");
+//  }
+//...................................................
+//  let age = "21";
+//  switch(age){
+//      case "age<18":
+//          console.log("child");
+//          break;
+//      case "18<age<40":
+//          console.log("adult");
+//          break;
+//      default:
+//          console.log("old age");
+//          break;  
+//  }
+//....................................................
+//  let day = "4";
+// switch(day){
+//     case "0":
+//         console.log("monday");
+//         break;
+//     case "1":
+//         console.log("tuesday");
+//         break;
+//     case "2":
+//         console.log("wednesday");
+//         break;
+//     case "3":
+//         console.log("thursday");
+//         break;
+//     case "4":
+//         console.log("friday");
+//         break;
+//     default:
+//         console.log("saturday");
+//         break;
+// }
+//..........................................
+
+//loops -->iteration statements --> for, while, do while, 
+//3 values
+//intialization
+//condition
+//increment//decrement
+
+//var a (declaring variable)
+//var a = 12; (intializing)
+ 
+// for(let i=1; i<=10; i++){ 
+//     console.log(i);
+// }
+
+for(let i=1; i<=10; i++){
+    console.log("19*" + i + "=" + 19*i);
+}
+
+
+
+
