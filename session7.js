2ba8c3790a938747878a70c0106ec03c7391e3a1
mouni <97968235+M0unika7@users.noreply.github.com>
2022-04-01 19:25:25 +0530

    Add files via upload

---
+++ session7.js
@@ -0,0 +1,158 @@
+// function handSubmit () {
+//     const value = document.getElementById("main");
+//     value.innerText("Mern stack")
+// }
+
+
+// let arr = [11, 23 , 3, 45 , 72, 68];
+
+// let g = `guests: mouni pravi bharath`
+// console.log(g);
+
+// let m = 'people:\n mouni\n uma\n neeru'
+// console.log(m)
+
+// let str = "hello - \i am continuing line2 - \ line3"
+// console.log(str)
+
+
+// let user = {
+//     age: 30,
+//     name: 'Rahul',
+//     sayHi: function () {
+//         console.log('hi!')
+//     }
+// }
+
+// function sayHi() {
+//     console.log("hi from normal function")
+// }
+// // a function bound to an object is called a method
+
+// sayHi()
+
+// console.log(user.sayHi)
+
+// user.sayHi()
+
+// let user = {
+//     age: 30,
+//     firstName: 'Rahul',
+//     sayHi: function () {
+//         console.log('hi', this.firstName)
+//     }
+// }
+
+// let admin = user
+// user = null
+// // console.log(admin.sayHi)
+// admin.sayHi()
+
+// let user = {
+//     firstName: 'rahul',
+//     score: 0,
+//     incrementScore() {
+//         this.score++
+//     }
+// }
+
+// // console.log(user.score)
+
+// user.incrementScore()
+// user.incrementScore()
+// user.incrementScore()
+
+// console.log(user.score)
+
+// var firstName = 'Aditi'
+// function sayHi() {
+//     console.log(this) //inside a normal function, "this" refers to the global object
+//     console.log(this.firstName)
+// }
+// let user = {
+//     firstName: 'rahul',
+//     objectType: 'USER'
+// }
+// let admin = {
+//     firstName: 'shruti',
+//     objectType: 'ADMIN'
+// }
+
+// let user2 = {
+//     firstName: 'Bhanu',
+//     sayHi: () => {
+//         console.log(this.firstName) //"this" inside an arrow function refers to the WINDOW object
+//     }
+//     // sayHi: function () {
+//     //     console.log(this.firstName) //"this" refers to user2
+//     // }
+// }
+
+// user.funcUser = sayHi
+// admin.funcAdmin = sayHi
+
+// user.funcUser()
+// admin.funcAdmin()
+
+//value of "this" is defined during runtime and it refers to the object from where it was called.
+
+// sayHi()
+
+// user2.sayHi()
+
+
+
+
+// question
+
+// let ladder = {
+//     step: 0,
+//     up() {
+//         // this.step++
+//         console.log(this.step++)
+//     },
+//     down() {
+//         this.step--
+//         console.log('decremented')
+//     },
+//     showStep() {
+//         console.log(this.step)
+//     }
+// }
+
+// ladder.up()
+// ladder.up()
+// ladder.down()
+// ladder.down()
+// ladder.down()
+// ladder.showStep()//-1
+// ladder.up()
+// ladder.up()
+// ladder.up()
+// ladder.showStep()//2
+
+
+// const obj = {
+//     message: 'Hello, World',
+//     getMessage() {
+//         const message = 'Hello, Earth'
+//         return this.message
+//     }
+// }
+
+// let result = obj.getMessage()
+// console.log(result) //
+
+
+// let obj = {
+//     who: 'World',
+//     greet: function () {
+//         console.log(`Hello, ${this.who}`)
+//     },
+//     farewell: () => {
+//         console.log(`Goodbye, ${this.who}`)
+//     }
+// }
+
+// obj.greet()
+// obj.farewell()
